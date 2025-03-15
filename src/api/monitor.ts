74a153d42dcc9eb032f2dc9240f75cff68f6b420
joshgideon1958 <josh.tetteh@pscgh.com>
2025-03-15 09:29:58 +0000

    updatee

--- src/api/monitor.ts
+++ src/api/monitor.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Monitor, CreateMonitorFormData, PaginatedResponse } from "../types/types";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = 'https://84bf-154-161-59-85.ngrok-free.app';
 
 export const createMonitor = async (data: CreateMonitorFormData): Promise<Monitor> => {
   const response = await axios.post(`${API_URL}/api/v1/monitor`, data, {
